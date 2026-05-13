34b5472f733d2cf4c590289a8dfe9f849b198f40
Herve Lagneaux <lagneaux.herve67@gmail.com>
2026-05-13 21:28:35 +0100

    EC2 and S3 are not working, i'm going to lose it

--- frontend/smart-event-planner/src/app/services/event.ts
+++ frontend/smart-event-planner/src/app/services/event.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Event } from '../event';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class EventService {
-  private apiUrl = 'http://localhost:5000/api/events';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ frontend/smart-event-planner/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = 
+{
+    production: true,
+    apiUrl: 'http://34.243.78.202:5000/api/events'
+};

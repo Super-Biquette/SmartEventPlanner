208e30a3dc94505ce49f3009403f817132db59e1
Herve Lagneaux <lagneaux.herve67@gmail.com>
2026-05-13 20:17:51 +0100

    finished event list component with filtering

--- frontend/smart-event-planner/src/app/components/event-list/event-list.ts
+++ frontend/smart-event-planner/src/app/components/event-list/event-list.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { EventService } from '../../services/event';
 import { Event } from '../../event';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-event-list',
   imports: [CommonModule,
-            RouterModule],
+            RouterModule,
+          FormsModule],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
 })
 export class EventList implements OnInit {
   events: Event[] = [];
+
+  searchText = '';
+
+  loading = true;
 
   constructor(private eventService: EventService) { }
 
@@ -20,6 +26,7 @@ export class EventList implements OnInit {
     this.eventService.getEvents().subscribe((data) => 
     {
       this.events = data;
+      this.loading = false;
       console.log(this.events);
     });
   }
@@ -30,5 +37,14 @@ export class EventList implements OnInit {
     {
     this.events = this.events.filter(event => event._id !== id);
     });
+  }
+
+  getFilteredEvents() 
+  {
+    return this.events.filter(event =>
+      event.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      event.category.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      event.location.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
 }
